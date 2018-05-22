431170e35940cf5a85cc9c4760b79cc3544899a9
dolfinn1212 <finn.coleman@hotmail.com>
2018-05-23 11:55:19 +1200

    Update sensor testing.cpp

--- Code/sensor testing.cpp
+++ Code/sensor testing.cpp
@@ -1,19 +1,18 @@
 #include <stdio.h>
 #include <time.h>
-#include “E101.h” // this is our custom ENGR 101 C library.
+#include “E101.h”
 int main (){
 	int adc_reading_right;
 	int adc_reading_left;
 	int adc_reading_front;
+	int i = 0;
 	init ();
-	for (int i = 0; i<100; i = i + 1) {
+	for (i = 0; i < 100; i = i + 1) {
 		adc_reading_right = read_analog(1);
 		adc_reading_left = read_analog(5);
 		adc_reading_front = read_analog(6);
 		printf("%d \t %d \t %d", adc_reading_left, adc_reading_front, adc_reading_right);
-		sleep1(0, 10000)
-
+		sleep1(0, 100000);
 	}
-	
 	return 0;
 } 
