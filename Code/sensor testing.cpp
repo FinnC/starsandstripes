bd3cec0bb62f096b0e02ac28742083d76a9f6c28
dolfinn1212 <finn.coleman@hotmail.com>
2018-05-23 11:19:57 +1200

    Update sensor testing.cpp

--- Code/sensor testing.cpp
+++ Code/sensor testing.cpp
@@ -2,36 +2,18 @@
 #include <time.h>
 #include “E101.h” // this is our custom ENGR 101 C library.
 int main (){
-
+	int adc_reading_right;
+	int adc_reading_left;
+	int adc_reading_front;
 	init ();
-	for (i = 0; i<10 i = i + 1){
-	   printf(“i =%d\n”,i);
-	   //We declare an integer variable to store the ADC data 
-		int adc_reading;
-		//Reads from Analog Pin 0 (A0) through A7 
-		adc_reading_right = read_analog(0);
-		//Prints read analog value 
-		printf(“  adc_r = %d\n”,adc reading_right);
-		//Waits for 1 seconds (50000000 microseconds)
-		sleep (5,0);
-		
-		
-		adc_reading_left =  read_analog(5);
-		//Prints read analog value 
-	    printf(“  adc_l = %d\n”,adc reading_left);
-		//Waits for 1 seconds (50000000 microseconds)
-		sleep (5,0);
-		
+	for (int i = 0; i<100; i = i + 1) {
+		adc_reading_right = read_analog(1);
+		adc_reading_left = read_analog(5);
 		adc_reading_front = read_analog(6);
-		//Prints read analog value for pin 6
-		printf(“   adc_f = %d\n”,adc reading_front);
-		//Waits for 1 seconds (50000000 microseconds)
-		sleep (5,0);
-		
-		
-		
-		
+		printf("%n \t %n \t %n", adc_reading_left, adc_reading_front, adc_reading_right);
+		sleep1(0, 10000)
+
 	}
 	
-	
-return 0;} 
+	return 0;
+} 
